f5964098f2bef284dc531cbd06294138e5f7b800
seydadilandursun <108757360+seydadilandursun@users.noreply.github.com>
2022-07-23 19:23:31 +0300

    forloop

---
+++ Forloop/Program.cs
@@ -0,0 +1,21 @@
+﻿//Ekrandan girilen sayiya kadar olan tek sayilarini ekrana yazir
+Console.WriteLine("Lutfen bir sayi giriniz:");
+int sayac=int.Parse(Console.ReadLine());
+for(int i=1;i<=sayac;i++)
+{
+  if(i%2==1)
+    Console.WriteLine(i);
+}
+
+//1 ile 1000 arasindaki tek ve cift sayilarin toplamlarini kendi icinde yazdir
+int tekToplam=0;
+int ciftToplam=0;
+for (int i = 0; i <=1000; i++)
+{
+    if(i%2 == 1)
+      tekToplam+=i;//tekToplam=tekToplam+i
+    else
+      ciftToplam+=i;
+}
+Console.WriteLine("Tektoplam="+tekToplam);
+Console.WriteLine("Cifttoplam="+ciftToplam);
